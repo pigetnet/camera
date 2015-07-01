eb0d69c5e985b2ee4d10cace20b729245d13f084
pigetnet <pigetnet@gmail.com>
2015-07-01 17:03:51 +0200

    JSON configuration manager

---
+++ python/readCameraJson.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import json
+import os.path
+import sys
+default_conf = '/opt/piget/camera/default/camera.json'
+user_conf = '/opt/user/config/camera/camera.json'
+pre_args = ""
+args = ""
+if(os.path.exists(user_conf)):
+	choose_conf = user_conf
+elif(os.path.exists(default_conf)):
+	choose_conf = default_conf
+else:
+	choose_conf = False
+
+#print choose_conf
+if choose_conf != False:
+	try:
+		with open(choose_conf) as config_file:
+			config = json.load(config_file)
+			for k,v in config.items():
+				if(k == "c"):
+					pre_args = pre_args + "--" + str(k) + " " + str(v) + " "
+				else:
+					args = args + "--" + str(k) + " " + str(v) + " "
+	except ValueError as e:
+		print e
+		exit(1)
+
+print pre_args
+print args
